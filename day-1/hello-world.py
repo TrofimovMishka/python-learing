e2ad052e71f9380ac2410f347b1731bcb3618ca0
Mykhailo Trofimov <Mykhailo.Trofimov@pl.ey.com>
2024-01-07 14:55:51 +0100

    Init day-1 learning about input and print, also added example about 'walrus' construction

---
+++ day-1/hello-world.py
@@ -0,0 +1,6 @@
+print("Welcome to the band generator")
+city = input("What the city that you grew up in? ")
+pet = input("What the name of pet that you have? ")
+print("The generated band is: " + city.upper() + ' ' + pet.lower())
+# This construction (:=) named: walrus (морж) since python 3.8
+print(opinion := input("Put your opinion there, this not matter for us :) "))
