1ef8a5d8c95a0b7a5857f76b9dbd2b1300329f5e
Mykhailo Trofimov <Mykhailo.Trofimov@pl.ey.com>
2024-01-14 15:33:33 +0100

    day-2 - Added method to check prime number

--- day-8/__init__.py
+++ day-8/__init__.py
@@ -5,3 +5,4 @@ def my_func(a, b, c):
     print(f'c = {c}')
 
 my_func(a="A hello Bob", c = "This is C", b = 'Bo-HO-HO') # Function invocation with param nameing!! Not order like in func definition
+
